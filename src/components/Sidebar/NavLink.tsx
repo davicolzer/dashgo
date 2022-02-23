aae72095b56a36f8dd63177bf6d16bc412884eb3
Davi Colzer Lira <davicolzer@gmail.com>
2022-02-23 00:58:56 -0300

    Correção do icone do sidemenu

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -5,7 +5,6 @@ import {
   Text,
 } from "@chakra-ui/react";
 import { ElementType } from "react";
-import { RiDashboardLine } from "react-icons/ri";
 import { ActiveLink } from "../ActiveLink";
 
 interface NavLinkProps extends ChakraLinkProps {
@@ -18,7 +17,7 @@ export function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
   return (
     <ActiveLink href={href} passHref>
       <ChakraLink display="flex" alignItems="center" {...rest}>
-        <Icon as={RiDashboardLine} fontSize="20" />
+        <Icon as={icon} fontSize="20" />
         <Text ml="4" fontWeight="medium">
           {children}
         </Text>
